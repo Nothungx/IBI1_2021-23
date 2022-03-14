7e8d8b2eaa6810ad9fa7d244fbe53a6d95add163
Nothungx <ziling.21@intl.zju.edu.cn>
2022-03-14 18:22:50 +0800

    Booleans

---
+++ Practical5/variables.py
@@ -0,0 +1,15 @@
+a=19245301
+b=4218520
+c=271
+d=b-c
+e=a-b
+if d<e:
+   print ("The rate of new cases is greater in 2021")
+else:
+   print ("The rate of new cases is greater in 2020")
+
+X=5<8
+Y=9>15
+W=X and Y
+print (W)
+
