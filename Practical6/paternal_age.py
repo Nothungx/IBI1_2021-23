b292d4e841741d1ba7137d4e5e1499a145babec2
Nothungx <ziling.21@intl.zju.edu.cn>
2022-03-23 12:59:48 +0800

    paternal_age.py_modified

--- Practical6/paternal_age.py
+++ Practical6/paternal_age.py
@@ -1,12 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-CHD={'30-year-old':1.03,'35-year-old':1.07,'40-year-old':1.11,'45-year-old':1.17,'50-year-old':1.23,'55-year-old':1.32,'60-year-old':1.42,'65-year-old':1.55,'70-year-old':1.72,'75-year-old':1.94}
+CHD={'30':1.03,'35':1.07,'40':1.11,'45':1.17,'50':1.23,'55':1.32,'60':1.42,'65':1.55,'70':1.72,'75':1.94}
+print (CHD)
 
 N=10 #There are 10 paternal ages corresponding to 10 CHD
 x=[30,35,40,45,50,55,60,65,70,75] #x's values show the paternal ages
-y=[1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94] #y's values shows the risk of CHD
+y=[1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94] #y's values shows the risk of CHD for the offspring corresponding to paternal ages
 plt.ylabel('CHD') #Set the y label
 plt.xlabel('paternal age') # Set the x label
 plt.scatter(x,y,marker='o')
 plt.show()
+
+age='55' #Age repesents the requested paternal age.
+print (CHD['55']) #Show the requested paternal age's corresponding CHD for the offspring.
