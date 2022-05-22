c527aa13e270a0f07c00c856478fa09aced915da
Nothungx <ziling.21@intl.zju.edu.cn>
2022-05-23 01:35:24 +0800

    practical9

---
+++ Practical9/count_cut_genes.py
@@ -0,0 +1,26 @@
+import re
+file_name=input("new file name is ")
+file_name_fa=file_name+'.fa'
+file = open('cut_genes.fa')
+fount=open(file_name_fa,'w')
+all_gene="s"
+
+for line in file:
+    line=line.rstrip()
+    all_gene=all_gene+line
+genes=re.split(r'>',all_gene)
+
+
+for i in range(1,len(genes)):
+    x=genes[i]
+    fragments= re.findall(r'GAATTC',x)
+    name=x[:7]
+    number=len(fragments)+1
+    DNA=re.findall(r'\'([A-Z]+)',x)
+    line1='>'+name+'   '
+    line2=str(number)+'\n'
+    line3=str(DNA)+'\n'
+    fount.write(line1)
+    fount.write(line2)
+    fount.write(line3)
+fount.close()
