4d51c9b6f6e37bc6635022a4556222c95703e925
Nothungx <101107212+Nothungx@users.noreply.github.com>
2022-05-24 03:33:05 +0800

    Delete Practical8 directory

--- Practical8/fragment_counter.py
+++
@@ -1,5 +0,0 @@
-import re
-seq="ATGCAATCGACTACGATCAATCGAGGGCC"
-print (seq)
-fragments= re.findall(r'GAATTC',seq)
-print (len(fragments), "fragment would be cut if EcoRI enzyme was applied to this sequence.")
